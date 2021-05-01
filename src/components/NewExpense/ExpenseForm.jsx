9189eb2df978c90f12c336c829d6c9b2daf8322f
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2021-05-01 17:34:57 +0900

    57 双方向バインディング　フォーム送信後、入力欄内容クリアされるようにした

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -45,6 +45,11 @@ const ExpenseForm = () => {
 			date: new Date(userInput.enteredDate),
 		};
 		console.log(expenseDate);
+		setUserInput({
+			enteredTitle: '',
+			enteredAmount: '',
+			enteredDate: '',
+		});
 	};
 
 	return (
@@ -52,15 +57,27 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+					<input
+						type="number"
+						min="0.01"
+						step="0.01"
+						value={userInput.enteredAmount}
+						onChange={amountChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+					<input
+						type="date"
+						min="2019-01-01"
+						max="2022-12-31"
+						value={userInput.enteredDate}
+						onChange={dateChangeHandler}
+					/>
 				</div>
 			</div>
 			<div className="new-expense__action">
