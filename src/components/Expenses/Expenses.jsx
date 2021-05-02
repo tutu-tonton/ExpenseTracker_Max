05b47ead60dbcbec48322ab491c0de67ef84d0ca
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2021-05-02 11:33:31 +0900

    65 配列をループしてJSX出力する場合、keyが必要。keyなしだと、配列全要素に対して再レンダリングされる。keyあれば、差分のみレンダリングされる。

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -24,7 +24,7 @@ const Expenses = (props) => {
 			<Card className="expenses">
 				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 				{props.items.map((expense) => (
-					<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+					<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
 				))}
 			</Card>
 		</div>
